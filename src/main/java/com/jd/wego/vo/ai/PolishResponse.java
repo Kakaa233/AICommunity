30f4b54831594c13f1f70193f8eb5851ef8ad592
paektu <15523135+paektu@user.noreply.gitee.com>
2026-05-11 00:21:52 +0800

    AIfix

--- src/main/java/com/jd/wego/vo/ai/PolishResponse.java
+++ src/main/java/com/jd/wego/vo/ai/PolishResponse.java
@@ -1,12 +1,19 @@
 package com.jd.wego.vo.ai;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+
+import java.util.List;
 
 /**
  * 文章润色响应
  */
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PolishResponse {
     private String polishedText;
     private String changes;
+    private String polishedTitle;
+    private String summary;
+    private List<String> tags;
 }
